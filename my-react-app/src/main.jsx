07584d9bc4ef1fe3f7489a80624d47a5e2c0541e
AlexanderDuya <alexduyaconnect@gmail.com>
2025-05-30 20:51:07 +0100

    test

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // Make sure this path is correct
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Make sure this path is correct
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App />);
